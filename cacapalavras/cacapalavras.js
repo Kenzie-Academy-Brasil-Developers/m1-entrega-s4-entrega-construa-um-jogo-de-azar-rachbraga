b2b23981f4c08fb1b5706773fd0936b8f68fee83
Marcelo Alves <marcelo.asf92@gmail.com>
2021-11-23 19:10:32 -0300

    Codigo funcional

--- cacapalavras/cacapalavras.js
+++ cacapalavras/cacapalavras.js
@@ -9,31 +9,35 @@ const arrayPalavras = [
     'pedra',
     'mesa',
     'sapato',
-    'copo',
-    'celular',
-    'caneta',
-    'agua',
-    'flor',
     'presunção',
+    'tela',
+    'kenzie',
     'ratificar',
     'paradigma',
     'humildade',
     'desdenhar',
     'dinamismo',
-    'adjacente',
+    'copo',
+    'celular',
+    'caneta',
+    'agua',
+    'flor',
+    'ar',
     'companhia',
     'interesse',
-    'tela',
-    'kenzie'
 ]
 
 // 2 - Fazer com que 3 palavras sejam selecionadas aleatóriamente 
 function palavrasAleatorias(array) {
     let palavras = [];
-    for (let i = 0; i < 3; i++) {
-        palavras.push(array[Math.floor(Math.random() * array.length)]);
+
+    while (palavras.length < 3) {
+        let posicao = array[Math.floor(Math.random() * array.length)];
+        if (palavras.indexOf(posicao) === -1) {
+            palavras.push(posicao);
+        }
+
     }
-    console.log(palavras)
     return palavras;
 }
 palavrasAleatorias(arrayPalavras);
@@ -54,6 +58,7 @@ const map = [
     ['', '', '', '', '', '', '', '', '', ''],
 ];
 
+// 4 - Fazer com que 3 palavras sejam colocadas aleatóriamente em um array bidmensional de 10x10
 function empurrar(palavra, x, y) {
     for (let i = 0; i < palavra.length; i++) {
         map[x][y] = palavra[i];
@@ -63,32 +68,46 @@ function empurrar(palavra, x, y) {
 
 let varControl = palavrasAleatorias(arrayPalavras);
 
-for (let c = 0; c < varControl.length; c++) {
+function adicionarPalavras() {
 
-    let palavra = varControl[c];
-    let linha = Math.floor(Math.random() * map.length);
-    let coluna = Math.floor(Math.random() * (map[0].length - palavra.length));
-    let guardarLinha = linha;
-    console.log(guardarLinha, linha)
-    if (guardarLinha === linha) {
+
+    let arrLinha = [];
+
+    while (arrLinha.length < 3) {
+        let linha = Math.floor(Math.random() * map.length);
+        if (arrLinha.indexOf(linha) === -1) {
+            arrLinha.push(linha);
+        }
+    }
+
+    for (let c = 0; c < varControl.length; c++) {
+
+        let palavra = varControl[c];
+        let linha = arrLinha[c];
+        let coluna = Math.floor(Math.random() * (map[0].length - palavra.length));
         empurrar(palavra, linha, coluna);
-    } else if (guardarLinha === linha) {
 
     }
 
 
 }
-
-
-
-
-
-
-
-
-
+adicionarPalavras();
 
 const divContainer = document.getElementById('container');
+const main = document.getElementById('main');
+
+function palavrasSelecionadas() {
+    const divMostrarPalavras = document.createElement('div');
+    divMostrarPalavras.classList.add('mostrarPalavras');
+    main.appendChild(divMostrarPalavras);
+    divMostrarPalavras.innerText = [
+        `Palavra 01: ${varControl[0].toUpperCase()}
+        Palavra 02: ${varControl[1].toUpperCase()}
+        Palavra 03: ${varControl[2].toUpperCase()}`
+    ]
+
+}
+palavrasSelecionadas()
 
 const buildLinha = (linhaAtual, linha) => {
     for (let coluna = 0; coluna < linhaAtual.length; coluna++) {
@@ -98,8 +117,8 @@ const buildLinha = (linhaAtual, linha) => {
         const div = document.createElement('div');
         div.setAttribute('coluna', coluna);
         div.setAttribute('linha', linha);
+        div.classList.add('divLetras')
         divContainer.appendChild(div);
-
 
         if (valorAtual === '') {
             div.innerText = letras.split('')[Math.floor(Math.random() * letras.length)];
@@ -111,9 +130,11 @@ const buildLinha = (linhaAtual, linha) => {
 
 
 
-    }
-}
 
+
+    }
+
+}
 
 const buildMap = () => {
     for (let linha = 0; linha < map.length; linha++) {
@@ -124,5 +145,26 @@ const buildMap = () => {
 buildMap();
 
 
+//5 - Informar ao usuário quando a palavra for clicada
 
+const eventOnClick = (e) => {
+    const target = e.target;
+    const coluna = target.getAttribute('coluna');
+    const linha = target.getAttribute('linha');
+    if (target.classList.contains('divLetras')) {
+        target.style.backgroundColor = 'blue';
+    }
+
+}
+divContainer.addEventListener('click', eventOnClick);
+
+const eventWithoutClick = (e) => {
+    console.log(e)
+    const target = e.target;
+    if (target.classList.contains('divLetras')) {
+        target.style.backgroundColor = 'yellow';
+    }
+
+}
+divContainer.removeEventListener('click', eventWithoutClick);
 
