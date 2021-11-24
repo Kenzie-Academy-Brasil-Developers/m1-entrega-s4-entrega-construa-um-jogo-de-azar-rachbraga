06287ef16af8d5dcd217e09c7e2bc1842dd6661b
Nieliton Rodrigues <nieliton.sousa3040@gmail.com>
2021-11-24 16:13:26 -0300

    Add audios

--- cacapalavras.html/java.js
+++ cacapalavras.html/java.js
@@ -4,17 +4,16 @@ Definir um array de 20 palavras com menos de 10 letras cada;
 
 Informar ao usuário quando uma palavra for clicada(encontrada); */
 
-const palavras=[20 palavras]
+const palavras=[20 /*palavras*/]
 
 /*Fazer com que 3 palavras sejam colocadas aleatóriamente em um 
 array bidmensional de 10x10 letras;*/
-
-let bidmensional= function gerarTabela(){
-    for (let linha=0; linha<10; linha++){
-        for (let coluna=0; coluna<10;coluna++){
-            
-        }
+// 
+let bidmensional = function gerarTabela(){
+    for (let linha = 0; linha < 10; linha++){
+        for (let coluna = 0; coluna < 10; coluna++){
     }
+}
 }
 
 /*Gerar letras aleatórias para completar esse array bidmensional;*/
